2ba0c6149e5d1ea0d6bcda5caa6764e9b9734654
Sumit Bilam <32146718+sumitbilam@users.noreply.github.com>
2019-11-16 18:51:10 -0500

    Add files via upload

---
+++ accelerometer.ino
@@ -0,0 +1,25 @@
+//define the pins
+const int xaxis = A3;
+const int yaxis = A2;
+const int zaxis = A1;
+
+void setup() {
+  // put your setup code here, to run once:
+  //initialize the serial communication
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //print the sensor values:
+  Serial.print(analogRead(xaxis));
+  Serial.print("\t");
+  Serial.print(analogRead(yaxis));
+  Serial.print("\t");
+  Serial.print(analogRead(zaxis));
+  Serial.println();
+
+  delay(3000);
+
+}
---
+++ slave.ino
@@ -0,0 +1,64 @@
+int receive_data;
+char received_str[20];
+char empty_str[20] = "";
+char conversion_buffer[5];
+int  count= 0;
+int x_flag = 0;
+int y_flag = 0;
+int x_print_flag = 0;
+int y_print_flag = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  //pinMode(receive_data, OUTPUT);
+  Serial.begin(38400);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    receive_data = Serial.read();
+    receive_data = receive_data - 48;
+    if (receive_data == -2){ //if '.'
+      count = 0;
+      x_flag = 1;
+    }
+    else if (receive_data == -15){ //if '!'
+      count = 0;
+      y_flag = 1;
+    }
+    else {
+      if (x_flag == 1){
+        received_str[0] = "\0";
+        x_flag = 0;
+        x_print_flag = 1;
+        y_print_flag = 0;
+        count = 0;
+      }
+      if (y_flag == 1){
+        received_str[0] = "\0";
+        y_flag = 0;
+        y_print_flag = 1;
+        x_print_flag = 0;
+        count = 0;
+      }
+      if (receive_data > -1){
+        itoa(receive_data, conversion_buffer, 10);
+        strcat(received_str, conversion_buffer);
+        count++;  
+      }
+      if (count == 3){
+        if (x_print_flag == 1){
+          Serial.print("x: ");
+        }
+        if (y_print_flag == 1){
+          Serial.print("y: ");
+        }
+        Serial.println(received_str);
+      }
+      delay(250);
+    } 
+  }
+}
+
