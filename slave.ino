8b901ddfd300f954769bbb9d7fbb6b36f6b598b7
Abinav Bukkaraya <11041625+Bukkaraya@users.noreply.github.com>
2019-11-17 21:11:43 -0500

    Update slave.ino

--- slave.ino
+++ slave.ino
@@ -1,46 +1,144 @@
+enum directions {
+  STRAIGHT,
+  REVERSE,
+  RIGHT,
+  LEFT,
+  STILL
+};
+
 int received_data;
 String conversion_buffer;
 int value;
-int x_value;
-int y_value;
+
+int x_change;
+int y_change;
+
+int BASE_X_ACCEL = 348;
+int BASE_Y_ACCEL = 348;
+
+int x_value = BASE_X_ACCEL;
+int y_value = BASE_Y_ACCEL;
+
+int TURN_THRESHOLD = 25;
+int LINEAR_THRESHOLD = 15;
+
+directions currentMovementDirection;
+
+int carSpeed = 200;
+
+
 
 void setup() {
-  // put your setup code here, to run once:
-  //pinMode(receive_data, OUTPUT);
   Serial.begin(38400);
 }
 
+
+
+// Design Choice: Turning has a higher priority
+// than going in a linear motion. As a result it,
+// has a higher threshold.
+
+
+// Have to figure out proper timing.
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available() > 0) {
-    received_data = Serial.read();
-    if(received_data == '.') {
-      Serial.print("Receiving Data...");
-      value = Serial.read();
-      while(value != '!') {
-        if(value == 'x') {
-          Serial.println("Value if for X");
-          conversion_buffer = Serial.readStringUntil(',');
-          x_value = conversion_buffer.toInt();
-        }
 
-        if(value == 'y') {
-          Serial.println("Reading Y Value:");
-          conversion_buffer = Serial.readStringUntil(',');
-          y_value = conversion_buffer.toInt();
-        }
+  resetValues();
 
-        value = Serial.read();
+  if(! (Serial.available() > 0)) {
+    return;
+  }
+  
+  
+  received_data = Serial.read();
+  if(received_data == '.') {
+    readPacket();
+  }
+
+
+  x_change = BASE_X_ACCEL - x_value;
+  y_change = BASE_Y_ACCEL - y_value;
+
+  currentMovementDirection = STILL;
+  
+  if (abs(y_change) > TURN_THRESHOLD) {
+    if(y_change < 0) {
+      currentMovementDirection = RIGHT;
+    } else {
+      currentMovementDirection = LEFT; 
+    }
+  } else {
+    if (abs(x_change) > LINEAR_THRESHOLD) {
+      if (x_change < 0) {
+        currentMovementDirection = REVERSE;
+      } else {
+        currentMovementDirection = STRAIGHT;
       }
-
-      Serial.print("X Value:");
-      Serial.println(x_value);
-      Serial.print("Y Value:");
-      Serial.println(y_value);
     }
   }
+
+  // TODO: Figure out a way to set the car speed
+  moveCar(currentMovementDirection, carSpeed);
+  
+  
+  
+}
+
+
+void moveCar(directions carDirection, int carSpeed) {
+  // Set the car speed here
+
+  switch(carDirection) {
+    case STRAIGHT:
+      Serial.println("Move car forward");
+      break;
+    case REVERSE:
+      Serial.println("Move car reverse");
+      break;
+    case RIGHT:
+      Serial.println("Move car right");
+      break;
+    case LEFT:
+      Serial.println("Move car left");
+      break;
+    case STILL:
+    default:
+      Serial.println("NOP");
+      break;
+  }
+
+  // Do this for 200ms and then stop
+    // car motion
+    // Stop car motion here
+  delay(50);
+    
+}
+
+
+void resetValues() {
+  x_value = BASE_X_ACCEL;
+  y_value = BASE_Y_ACCEL;
 }
 
 void readPacket() {
-  return;
+  Serial.println("Receiving Data...");
+  value = Serial.read();
+  while(value != '!') {
+    if(value == 'x') {
+      conversion_buffer = Serial.readStringUntil(',');
+      x_value = conversion_buffer.toInt();
+    }
+
+    if(value == 'y') {
+      conversion_buffer = Serial.readStringUntil(',');
+      y_value = conversion_buffer.toInt();
+    }
+
+    value = Serial.read();
+  }
+
+  Serial.print("X Value:");
+  Serial.println(x_value);
+  Serial.print("Y Value:");
+  Serial.println(y_value);
 }
